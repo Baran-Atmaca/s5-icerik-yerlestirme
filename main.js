4376026a3d0b3fd10a697050e0cd9c67cbe67503
Baran Atmaca <atmacabaran161@gmail.com>
2026-09-17 15:43:24 +0300

    Update main.js

--- main.js
+++ main.js
@@ -33,3 +33,39 @@ linkler[4].classList.add('italic');
 const ctaResim = document.getElementById('cta-img');
 ctaResim.src = './images/cta-img.webp';
 
+const topBasliklar = document.querySelectorAll('.top-content h4');
+topBasliklar[0].textContent = siteContent['top-content']['left-h4'];
+topBasliklar[1].textContent = siteContent['top-content']['right-h4'];
+
+const topParagraflar = document.querySelectorAll('.top-content p');
+topParagraflar[0].textContent = siteContent['top-content']['left-content'];
+topParagraflar[1].textContent = siteContent['top-content']['right-content'];
+
+const middleImg = document.getElementById('middle-img');
+middleImg.src = './images/graphs.webp';
+
+const botBasliklar = document.querySelectorAll('.bottom-content h4');
+botBasliklar[0].textContent = siteContent['bottom-content']['left-h4'];
+botBasliklar[1].textContent = siteContent['bottom-content']['middle-h4'];
+botBasliklar[2].textContent = siteContent['bottom-content']['right-h4'];
+
+const botParagraflar = document.querySelectorAll('.bottom-content p');
+botParagraflar[0].textContent = siteContent['bottom-content']['left-content'];
+botParagraflar[1].textContent = siteContent['bottom-content']['middle-content'];
+botParagraflar[2].textContent = siteContent['bottom-content']['right-content'];
+
+const contactBaslik = document.querySelector('.contact h4');
+contactBaslik.textContent = siteContent.contact['contact-h4'];
+
+const contactParagraflari = document.querySelectorAll('.contact p');
+
+contactParagraflari[0].textContent = siteContent.contact['address'];
+contactParagraflari[1].textContent = siteContent.contact['phone'];
+contactParagraflari[2].textContent = siteContent.contact['email'];
+
+const footerLink = document.querySelector('footer a');
+
+footerLink.textContent = siteContent.footer['copyright'];
+footerLink.classList.add('bold');
+
+
